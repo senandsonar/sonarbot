074478e77fc08a441eb97af6d3c1ba07ed42a3dd
Luke Paulic <lukepaulic@gmail.com>
2020-05-26 16:05:54 -0400

    First
    First code sent to GitHub for Sonar discord bot.

---
+++ commands/8ball.js
@@ -0,0 +1,40 @@
+const {MessageEmbed} = require('discord.js')
+module.exports={
+    name: "8ball",
+    description: "There is a big chance I insult you!",
+    category: "fun",
+    run: async(client,message,args)=>{
+        let question = message.content.slice(7)
+        if(!question)return message.channel.send(`You did not specify your question!`)
+        else{
+            let responses=[
+                    "It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes - definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful."
+            ]
+            let response = responses[Math.floor(Math.random()*(responses.length)-1)]
+            let Embed = new MessageEmbed()
+            .setTitle(`8Ball!`)
+            .setDescription(`Your question: ${question}\nMy reply: ${response}`)
+            .setColor(`RANDOM`)
+            message.channel.send(Embed)
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const config = require('./config.json');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('ready', () => {
+    client.user.setActivity('Made by Sen#1143! Sonar is a multi-purpose bot for       miscellaneous commands.', { type: 'PLAYING' });
+    
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    var randomnumber=Math.floor(Math.random()*101);
+    
+    switch (command) {
+        case `ping`:
+            client.commands.get('ping').execute(message, args);
+            break;
+        case `beep`:
+            message.channel.send('Boop.');
+            break;
+        case `userinfo`:
+            message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+            break;
+        case `number`:
+            message.channel.send(randomnumber);
+            break;
+        case `corona`:
+            client.commands.get('corona').run(client, message, args);
+            break;
+        case `kick`:
+            client.commands.get('kick').run(client, message, args);
+            break;
+        case `8ball`:
+            client.commands.get('8ball').run(client, message, args);
+            break;
+         case `help`:
+            client.commands.get('help').execute(message, args);
+            break;
+         case `mute`:
+            client.commands.get('mute').run(client, message, args);
+            break;
+        case `unmute`:
+            client.commands.get('unmute').run(client, message, args);
+            break;
+        default:
+            break;
+    }
+});
+
+client.on("message", async message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g) //arguments
+    const command = args.shift().toLowerCase(); //command
+
+    if (message.content.indexOf(prefix) !== 0) return;
+    
+    if (command == "av") {
+
+    var user;
+    var embed = new Discord.MessageEmbed();
+    user = message.mentions.users.first(); //mentioned user, if any
+    if (!user) { //if no one is mentioned
+            user = message.author;
+            embed.setColor("RANDOM"); //can specifiy color of embed here
+            embed.setImage(user.displayAvatarURL());
+            message.channel.send(embed);
+    } else { //if a user id IS specified (need developer mode on on discord to get it)
+            user = message.mentions.users.first();
+            embed.setColor("RANDOM"); //can specifiy color of embed here
+            embed.setImage(user.displayAvatarURL());
+            message.channel.send(embed);
+        }
+    }
+})
+
+
+
+client.login(token);
