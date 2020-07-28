57dc5911a01f96c3b629629f753aa8e638cb75cb
Senla <lukepaulic@gmail.com>
2020-07-28 19:31:02 +0800

    made a few small changes

--- commands/ping.js
+++ commands/ping.js
@@ -13,8 +13,7 @@ module.exports = {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
          message.channel.send(`🏓 Pinging....`).then(msg=>{
         const _ = new Discord.MessageEmbed()
-        .setTitle('Pong!')
-        .setDescription(`🏓 Pong!\n> Latency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\n> \n> API Latency is ${Math.round(bot.ws.ping)}ms`)
+        .setDescription(`🏓 **Pong!**\n> Latency is \`${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\`\n> \n> API Latency is \`${Math.round(bot.ws.ping)}ms\``)
         .setColor(`#faf6f6`)
         .setAuthor(user.user.username, user.user.displayAvatarURL({ dynamic: true }))
         msg.edit(_);
